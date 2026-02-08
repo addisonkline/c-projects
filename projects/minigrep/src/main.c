cb049482847414cf6168872305ba401f8644b735
addisonkline <klineaddison@gmail.com>
2026-02-07 20:07:01 -0500

    minigrep verbosity flag

--- projects/minigrep/src/main.c
+++ projects/minigrep/src/main.c
@@ -6,7 +6,8 @@
 
 int count_pattern_in_line(
     const char* line,
-    const char* pattern
+    const char* pattern,
+    int verbose
 ) {
     int len_line = strlen(line);
     int len_pattern = strlen(pattern);
@@ -24,6 +25,9 @@ int count_pattern_in_line(
             idx_pattern++;
 
             if (len_equal == len_pattern) {
+                if (verbose) {
+                    printf("> > found occurrence at column %i\n", idx_line);
+                }
                 count++;
                 len_equal = 0;
             }
@@ -42,7 +46,8 @@ int count_pattern_in_line(
 int count_pattern_in_file(
     const char* file,
     const char* pattern,
-    int get_line_numbers
+    int get_line_numbers,
+    int verbose
 ) {
     FILE* stream;
     char* line = NULL;
@@ -56,8 +61,17 @@ int count_pattern_in_file(
     }
 
     int count = 0;
+    int line_num = 0;
     while ((nread = getline(&line, &size, stream)) != -1) {
-        count += count_pattern_in_line(line, pattern);
+        int line_count = count_pattern_in_line(line, pattern, verbose);
+        line_num++;
+        count += line_count;
+        if (line_count > 0 && get_line_numbers) {
+            printf("> found %i occurrences in line %i\n", line_count, line_num);
+        }
+        else if (verbose) {
+            printf("> found %i occurrences in line %i\n", line_count, line_num);
+        }
     }
 
     free(line);
@@ -69,18 +83,21 @@ int count_pattern_in_file(
 int main(int argc, char *argv[])
 {
     int line_numbers = 0;
+    int verbose = 0;
     char* pattern;
 
     static struct option longopts[] = {
         {"line-number",  no_argument,       0, 'n'},
+        {"verbose", no_argument, 0, 'v'},
         {"pattern",      required_argument, 0, 'e'},
         {0, 0, 0, 0}
     };
 
     int opt;
-    while ((opt = getopt_long(argc, argv, "ne:", longopts, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "nve:", longopts, NULL)) != -1) {
         switch (opt) {
             case 'n': line_numbers = 1; break;
+            case 'v': verbose = 1; break;
             case 'e': pattern = optarg; break;
             default:
                 fprintf(stderr, "Usage: ...\n");
@@ -88,12 +105,16 @@ int main(int argc, char *argv[])
         }
     }
 
-    printf("line_numbers = %d\n", line_numbers);
-    printf("===\n");
+    if (verbose) {
+        printf("line_numbers = %d\n", line_numbers);
+        printf("pattern = '%s'\n", pattern);
+        printf("===\n");
+    }
+    
     for (int i = optind; i < argc; i++) {
         printf("reading file %s...\n", argv[i]);
-        int result = count_pattern_in_file(argv[i], pattern, line_numbers);
-        printf("found %i occurrences\n", result);
+        int result = count_pattern_in_file(argv[i], pattern, line_numbers, verbose);
+        printf("> TOTAL: %i occurrences\n", result);
     }
     return 0;
 }
